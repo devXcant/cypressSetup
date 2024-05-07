cb2c2c661bbb3603f9a2c6a33d26debe2ec25444
devXcant <devxcant@gmail.com>
2024-05-07 12:42:09 +0100

    commit

--- cypress/e2e/test2.cy.js
+++ cypress/e2e/test2.cy.js
@@ -1,9 +1,6 @@
 describe('chnp', ()=> {
     it('auth', ()=> {
-        cy.visit('https://chnp-frontend.onrender.com')
-        cy.get('input[type=email]').type('superadmin@chnp.com')
-        cy.get('input[type=password]').type('Password123')
-        cy.get('.mt-6 > .w-full').click()
-        cy.url({timeout: 30000}).should('include','/admin')
+        cy.visit('http://eservices.showcase.com.ng/home-page/dashboard')
+        
     })
 })
